3f94d613ae9789f8f48a6827c32a5ed5d2ac23ad
danil732000@gmail.com <danil732000@gmail.com>
2020-05-22 11:22:53 +0400

    6

--- BlacksmithWorkshop/BlacksmithWorkshopView/Program.cs
+++ BlacksmithWorkshop/BlacksmithWorkshopView/Program.cs
@@ -32,7 +32,8 @@ namespace BlacksmithWorkshopView
 			currentContainer.RegisterType<IBilletsLogic, BilletsLogic>(new HierarchicalLifetimeManager());
 			currentContainer.RegisterType<MainLogic>(new HierarchicalLifetimeManager());
 			currentContainer.RegisterType<ReportLogic>(new HierarchicalLifetimeManager());
-			currentContainer.RegisterType<IClientLogic, ClientLogic>(new HierarchicalLifetimeManager());
+			currentContainer.RegisterType<IClientLogic, ClientLogic>(new
+		  HierarchicalLifetimeManager());
 			currentContainer.RegisterType<IImplementerLogic, ImplementerLogic>(new HierarchicalLifetimeManager());
 			return currentContainer;
 		}
